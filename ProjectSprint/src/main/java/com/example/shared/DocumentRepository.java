196750a7613a5b289310ac959857d1a1028b5eac
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-11-20 11:42:28 +0530

    Delete ProjectSprint directory

--- ProjectSprint/src/main/java/com/example/shared/DocumentRepository.java
+++
@@ -1,9 +0,0 @@
-
-//DocumentRepository.java
-package com.example.shared;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DocumentRepository extends JpaRepository<Document, Long> {
- // Add custom queries if needed
-}
